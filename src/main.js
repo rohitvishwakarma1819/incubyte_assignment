5468c2d0abe4cf642dbfcd0da929ff216397fe13
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2024-12-30 09:53:39 +0530

    feat(service): Update add function to handle newline delimiter along with comma

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ export function add(numbers) {
   if (numbers === "") {
     return 0;
   }
-  const nums = numbers.split(",");
+  const nums = numbers.split(/[\n,]/);
   const result = nums.reduce((prev, curr) => prev + parseInt(curr, 10), 0);
   return result;
 }
