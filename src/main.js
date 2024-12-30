32b9f3b05eab4c7c5165f2334610b6b50c51044e
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2024-12-30 09:43:09 +0530

    feat(service): Update add function to handle two number input with comma delimiter

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ export function add(numbers) {
   if (numbers === "") {
     return 0;
   }
-  return parseInt(numbers, 10);
+  const nums = numbers.split(",");
+  const result = nums.reduce((prev, curr) => prev + parseInt(curr, 10), 0);
+  return result;
 }
