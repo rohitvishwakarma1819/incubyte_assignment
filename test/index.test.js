7d8d7dd44b3efc328f163608298037c23321b1ce
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2024-12-30 09:49:44 +0530

    test(service):  Add test for adding multiple numbers

--- test/index.test.js
+++ test/index.test.js
@@ -10,4 +10,10 @@ test("should return 1 when given '1'", () => {
 
 test("should return sum of two numbers when given two numbers", () => {
   expect(add("1,2")).toBe(3);
+  expect(add("1,3")).toBe(4);
+});
+
+test("should return sum of multiple numbers when given multiple numbers", () => {
+  expect(add("1,2,3,4,5")).toBe(15);
+  expect(add("1,2,3,4,5,6,7,8,9,10")).toBe(55);
 });
