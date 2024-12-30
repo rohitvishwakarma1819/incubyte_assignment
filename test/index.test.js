9c6578e2a93fe9bd819ee9b12ec3dd76a00dafb4
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2024-12-30 09:36:36 +0530

    feat(test): add test to expect single number that returns itself

--- test/index.test.js
+++ test/index.test.js
@@ -3,3 +3,7 @@ import { add } from "../src/index";
 test("should return 0 when given an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("should return 1 when given '1'", () => {
+  expect(add("1")).toBe(1);
+});
