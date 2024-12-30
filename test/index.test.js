4a663ca0017fc79ef6d6a7a63626b3f1ae9d62c7
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2024-12-30 09:51:43 +0530

    test(service): Add test for adding multiple numbers with newline delimiter

--- test/index.test.js
+++ test/index.test.js
@@ -17,3 +17,8 @@ test("should return sum of multiple numbers when given multiple numbers", () => 
   expect(add("1,2,3,4,5")).toBe(15);
   expect(add("1,2,3,4,5,6,7,8,9,10")).toBe(55);
 });
+
+test("should return sum of two numbers when given two numbers separated by new line", () => {
+  expect(add("1\n2")).toBe(3);
+  expect(add("1\n3")).toBe(4);
+});
