39194f480babcce30cea86fc308f139b2730bee4
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2025-01-27 21:36:40 +0530

    refactor(service): refactor delimiter extraction logic
    Refactor delimiter extraction logic and add JSDoc comments for better readability and maintainability

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 /**
- * Adds a list of numbers provided as a string.
+ * calculate a list of numbers provided as a string.
  *
  * This function supports custom delimiters, specified at the beginning of the string in the format:
  * `//<delimiter>\n<numbers>`.
@@ -7,33 +7,62 @@
  * - Default delimiters are `,` and `\n`.
  * - Custom delimiters can be any single character.
  * - If the custom delimiter is `-`, the function processes it uniquely to avoid conflicts with negative numbers.
+ * - If the custom delmiter is `*`, the function returns product of all the numbers
  *
  * @param {string} numbers - A string of numbers separated by delimiters.
  *   Example: "1,2,3" or "//;\n1;2;3".
  *
  * @throws {Error} If the string contains negative numbers, an error is thrown listing the negatives.
  *
- * @returns {number} The sum of the numbers in the string. Returns 0 if the input string is empty.
+ * @returns {number} The sum or product of the numbers in the string. Returns 0 if the input string is empty.
  *
  * @example
- * add("1,2,3"); // Returns 6
- * add("//;\n1;2;3"); // Returns 6
- * add(""); // Returns 0
- * add("//-\n1-2-3"); // Returns 6
- * add("1,-2,3"); // Throws Error: negatives numbers not allowed -2
+ * stringCalculation("1,2,3"); // Returns 6
+ * stringCalculation("//;\n1;2;3"); // Returns 6
+ * stringCalculation(""); // Returns 0
+ * stringCalculation("//-\n1-2-3"); // Returns 6
+ * stringCalculation("1,-2,3"); // Throws Error: negatives numbers not allowed -2
  */
-export function add(numbers) {
+export function stringCalculation(numbers) {
   if (numbers === "") {
     return 0;
   }
 
+  const { delimiter, processedNumbers } = extractDelimiter(numbers);
+  const nums = processedNumbers.split(delimiter).map((num) => parseInt(num, 10));
+  const negatives = nums.filter((num) => num < 0);
+  if (negatives.length) {
+    throw new Error(`negatives numbers not allowed ${negatives.join(", ")}`);
+  }
+  const result = nums.reduce((prev, curr) => {
+    return calculateResult(prev, curr, delimiter);
+  });
+
+  return result;
+}
+
+/**
+ * Extracts the delimiter and processes the numbers string.
+ *
+ * The function supports custom delimiters specified at the beginning of the string
+ * in the format: `//<delimiter>\n<numbers>`.
+ *
+ * - Default delimiters are `,` and `\n`.
+ * - Custom delimiters can be any single character.
+ * - If the custom delimiter is `-`, the function processes it uniquely to avoid conflicts with negative numbers.
+ *
+ * @param {string} numbers - A string of numbers separated by delimiters.
+ * @returns {Object} An object containing the delimiter and the processed numbers string.
+ * @property {string|RegExp} delimiter - The delimiter used to separate the numbers.
+ * @property {string} processedNumbers - The processed numbers string.
+ */
+function extractDelimiter(numbers) {
   let delimiter = /[\n,]/; // default delimiters
   if (numbers.startsWith("//")) {
     const parts = numbers.split("\n");
     delimiter = parts[0].slice(2);
     numbers = parts[1];
 
-    // If the delimiter is `-`, replace all occurrences of `--` with `-`
     if (delimiter === "-") {
       numbers = numbers.replace(/--/g, "#");
       numbers = numbers.replace(/-/g, "|");
@@ -41,19 +70,21 @@ export function add(numbers) {
       delimiter = /\|/;
     }
   }
-  const nums = numbers.split(delimiter).map((num) => parseInt(num, 10));
-  const negatives = nums.filter((num) => num < 0);
-  if (negatives.length) {
-    throw new Error(`negatives numbers not allowed ${negatives.join(", ")}`);
-  }
-  const result = nums.reduce((prev, curr) => {
-    return solve(prev, curr, delimiter);
-  }, getDefaultValue(delimiter));
-
-  return result;
+  return { delimiter, processedNumbers: numbers };
 }
 
-function solve(prev, curr, delimiter) {
+/**
+ * Calculates the result of two numbers based on the delimiter.
+ *
+ * If the delimiter is '*', it returns the product of the two numbers.
+ * Otherwise, it returns the sum of the two numbers.
+ *
+ * @param {number} prev - The previous number in the calculation.
+ * @param {number} curr - The current number in the calculation.
+ * @param {string|RegExp} delimiter - The delimiter used to separate the numbers.
+ * @returns {number} The result of the calculation.
+ */
+function calculateResult(prev, curr, delimiter) {
   if (delimiter === '*') {
     return prev * curr;
   }
@@ -61,6 +92,3 @@ function solve(prev, curr, delimiter) {
   return prev + curr;
 }
 
-function getDefaultValue(delimiter) {
-  return delimiter === '*' ? 1 : 0;
-}
--- test/index.test.js
+++ test/index.test.js
@@ -1,51 +1,51 @@
-import { add } from "../src/index";
+import { stringCalculation } from "../src/index";
 
 test("should return 0 when given an empty string", () => {
-  expect(add("")).toBe(0);
+  expect(stringCalculation("")).toBe(0);
 });
 
 test("should return 1 when given '1'", () => {
-  expect(add("1")).toBe(1);
+  expect(stringCalculation("1")).toBe(1);
 });
 
 test("should return sum of two numbers when given two numbers", () => {
-  expect(add("1,2")).toBe(3);
-  expect(add("1,3")).toBe(4);
+  expect(stringCalculation("1,2")).toBe(3);
+  expect(stringCalculation("1,3")).toBe(4);
 });
 
 test("should return sum of multiple numbers when given multiple numbers", () => {
-  expect(add("1,2,3,4,5")).toBe(15);
-  expect(add("1,2,3,4,5,6,7,8,9,10")).toBe(55);
+  expect(stringCalculation("1,2,3,4,5")).toBe(15);
+  expect(stringCalculation("1,2,3,4,5,6,7,8,9,10")).toBe(55);
 });
 
 test("should return sum of two numbers when given two numbers separated by new line", () => {
-  expect(add("1\n2")).toBe(3);
-  expect(add("1\n3")).toBe(4);
+  expect(stringCalculation("1\n2")).toBe(3);
+  expect(stringCalculation("1\n3")).toBe(4);
 });
 
 test("should return sum of multiple numbers having both comma and newline delimiters", () => {
-  expect(add("1\n2,3")).toBe(6);
-  expect(add("1\n3,4,5")).toBe(13);
+  expect(stringCalculation("1\n2,3")).toBe(6);
+  expect(stringCalculation("1\n3,4,5")).toBe(13);
 });
 
 test("should return sum of multiple numbers having custom delimiter", () => {
-  expect(add("//;\n1;2")).toBe(3);
-  expect(add("//#\n1#3#4")).toBe(8);
-  expect(add("//|\n1|3|5")).toBe(9);
+  expect(stringCalculation("//;\n1;2")).toBe(3);
+  expect(stringCalculation("//#\n1#3#4")).toBe(8);
+  expect(stringCalculation("//|\n1|3|5")).toBe(9);
 });
 
 test("should throw an error when given negative numbers", () => {
-  expect(() => add("-1")).toThrow("negatives numbers not allowed -1");
-  expect(() => add("-1,2,-3")).toThrow("negatives numbers not allowed -1, -3");
+  expect(() => stringCalculation("-1")).toThrow("negatives numbers not allowed -1");
+  expect(() => stringCalculation("-1,2,-3")).toThrow("negatives numbers not allowed -1, -3");
 });
 
 test("should throw an error when given negative numbers if delimiter is also - symbol", () => {
-  expect(() => add("-1")).toThrow("negatives numbers not allowed -1");
-  expect(() => add("//-\n1,--2,-3")).toThrow(
+  expect(() => stringCalculation("-1")).toThrow("negatives numbers not allowed -1");
+  expect(() => stringCalculation("//-\n1,--2,-3")).toThrow(
     "negatives numbers not allowed -2"
   );
 });
 
 test("multiply number if delimiter is *", () => {
-  expect(add("//*\n1*3*4")).toBe(12);
+  expect(stringCalculation("//*\n2*3*4")).toBe(24);
 });
